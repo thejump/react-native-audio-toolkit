c093d03dbd8653d5d01f740487a459924335e34a
jcharbo <jcharbo@users.noreply.github.com>
2017-09-18 10:18:24 -0500

    Update AudioPackage.java

--- android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioPackage.java
+++ android/lib/src/main/java/com/futurice/rctaudiotoolkit/AudioPackage.java
@@ -19,10 +19,6 @@ public class AudioPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
